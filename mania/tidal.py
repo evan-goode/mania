56df3766e58420eb56ee3e1f949ae09c360e7af9
Corewala <35401197+Corewala@users.noreply.github.com>
2021-05-04 16:51:19 -0400

    Split the authentication function so that each component (getting user/device codes and authenticating) can be called individually. (#11)
    * Split the authentication function so that each component (getting user/device codes and authenticating) can be called individually.

--- mania/tidal.py
+++ mania/tidal.py
@@ -1,3 +1,4 @@
+
 """Tidal authentication and API client"""
 
 from operator import itemgetter
@@ -42,14 +43,14 @@ class TidalSession:
     Daniel!"""
 
     def __init__(
-        self,
-        device_code: Optional[str] = None,
-        user_code: Optional[str] = None,
-        country_code: Optional[str] = None,
-        access_token: Optional[str] = None,
-        refresh_token: Optional[str] = None,
-        user_id: Optional[str] = None,
-        expires: Optional[datetime.datetime] = None,
+            self,
+            device_code: Optional[str] = None,
+            user_code: Optional[str] = None,
+            country_code: Optional[str] = None,
+            access_token: Optional[str] = None,
+            refresh_token: Optional[str] = None,
+            user_id: Optional[str] = None,
+            expires: Optional[datetime.datetime] = None,
     ):
         self.device_code = device_code
         self.user_code = user_code
@@ -90,9 +91,7 @@ class TidalSession:
         except AssertionError as error:
             raise TidalAuthError("You need a HiFi subscription.") from error
 
-    def authenticate(self) -> None:
-        """Authenticate as a new linked device"""
-        # retrieve csrf token for subsequent request
+    def get_authorization(self):
         authorization_response = self._session.post(
             f"{AUTH_ENDPOINT}/oauth2/device_authorization",
             data={
@@ -100,7 +99,6 @@ class TidalSession:
                 "scope": "r_usr w_usr",
             },
         )
-
         if authorization_response.status_code == 400:
             raise TidalAuthError(
                 "Authorization failed! Is the clientid/token up to date?"
@@ -113,6 +111,8 @@ class TidalSession:
         authorization_json = authorization_response.json()
         self.device_code = authorization_json["deviceCode"]
         self.user_code = authorization_json["userCode"]
+
+    def authenticate(self) -> None:
         print(
             "Go to https://link.tidal.com/{} and log in or sign up to TIDAL.".format(
                 self.user_code
@@ -209,12 +209,12 @@ class TidalSession:
         }
 
     def request(
-        self,
-        method: str,
-        path: str,
-        params: Optional[dict] = None,
-        data: Optional[dict] = None,
-        attempt: int = 1,
+            self,
+            method: str,
+            path: str,
+            params: Optional[dict] = None,
+            data: Optional[dict] = None,
+            attempt: int = 1,
     ) -> requests.models.Response:
         """Make a request to TIDAL API"""
 
@@ -240,8 +240,8 @@ class TidalSession:
         except requests.exceptions.HTTPError as error:
             status = error.response.status_code
             if (
-                status == 429
-                or (status == 500 and error.response.json().get("subStatus") == 999)
+                    status == 429
+                    or (status == 500 and error.response.json().get("subStatus") == 999)
             ) and attempt < MAXIMUM_ATTEMPTS:
                 # backoff and retry if we receive a 429 Client Error: Too Many Requests for url
                 time.sleep(2 ** attempt)
@@ -313,20 +313,20 @@ class TidalClient(Client):
         return f"https://resources.tidal.com/images/{cover.replace('-', '/')}/{COVER_ART_SIZE}x{COVER_ART_SIZE}.jpg"
 
     def _request(
-        self,
-        method: str,
-        path: str,
-        params: Optional[dict] = None,
-        data: Optional[dict] = None,
+            self,
+            method: str,
+            path: str,
+            params: Optional[dict] = None,
+            data: Optional[dict] = None,
     ) -> requests.models.Response:
         return self._tidal_session.request(method, path, params, data)
 
     def _paginate(
-        self,
-        method: str,
-        path: str,
-        params: Optional[dict] = None,
-        data: Optional[dict] = None,
+            self,
+            method: str,
+            path: str,
+            params: Optional[dict] = None,
+            data: Optional[dict] = None,
     ) -> List[Any]:
         items = []
         params = {
@@ -348,7 +348,7 @@ class TidalClient(Client):
         quality_levels = {"low": 1, "high": 2, "lossless": 3, "master": 4}
 
         special_audio_modes = (
-            frozenset(tidal_object.get("audioModes", ())) & SPECIAL_AUDIO_MODES
+                frozenset(tidal_object.get("audioModes", ())) & SPECIAL_AUDIO_MODES
         )
 
         if tidal_object["audioQuality"] == "LOSSLESS" or special_audio_modes:
@@ -411,7 +411,7 @@ class TidalClient(Client):
         )
 
     def _tidal_track_to_track(
-        self, tidal_track: dict, album: Optional[Album] = None
+            self, tidal_track: dict, album: Optional[Album] = None
     ) -> Track:
         # we can be pretty sure that an album ID is valid if it comes from TIDAL
         album = album or cast(Album, self.get_album_by_id(tidal_track["album"]["id"]))
@@ -444,10 +444,10 @@ class TidalClient(Client):
         )
 
     def search(
-        self,
-        query: str,
-        media_type: Type[Union[Track, Album, Artist]],
-        count: int,
+            self,
+            query: str,
+            media_type: Type[Union[Track, Album, Artist]],
+            count: int,
     ) -> List[Union[Track, Album, Artist]]:
         types, key, resolver = {
             Track: ("TRACKS", "tracks", self._tidal_track_to_track),
